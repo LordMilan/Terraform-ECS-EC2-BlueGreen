1a31f0680e4ac811c71235e24c572fafefb6f616
LordMilan <milan.mahat.739@gmail.com>
2023-04-10 12:45:29 +0545

    Service defn changed

--- service-defn.tf
+++ service-defn.tf
@@ -1,23 +1,24 @@
 ## Service for ECS Cluster(APP) is defined here ##
 ## APP ##
-resource "aws_ecs_service" "service_app" {
+
+resource "aws_ecs_service" "ecs_service" {
   name            = local.service_name_app
   cluster         = aws_ecs_cluster.web-cluster.id
   task_definition = aws_ecs_task_definition.task-definition-app.arn
   desired_count   = local.container_numbers_app
-  ordered_placement_strategy {
-    type  = "binpack"
-    field = "cpu"
+  deployment_controller {
+    type = "CODE_DEPLOY"
   }
   load_balancer {
     target_group_arn = aws_lb_target_group.lb_target_group_app.arn
     container_name   = local.container_name_app
     container_port   = local.container_port_app
   }
-  # Optional: Allow external changes without Terraform plan difference(for example ASG)
   lifecycle {
-    ignore_changes = [desired_count]
+    ignore_changes = [
+      load_balancer,
+      desired_count,
+      task_definition
+    ]
   }
-  launch_type = "EC2"
-  depends_on  = [aws_lb_target_group.lb_target_group_app]
 }
