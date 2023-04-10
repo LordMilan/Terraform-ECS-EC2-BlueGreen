08754d5449bb66a5ffb660ffd2718047b300b280
LordMilan <milan.mahat.739@gmail.com>
2023-04-10 13:41:23 +0545

    Code Deploy Added

--- iam.tf
+++ iam.tf
@@ -51,7 +51,7 @@ resource "aws_iam_instance_profile" "ecs_service_role" {
 }
 
 # create a service role for codedeploy
-resource "aws_iam_role" "codedeploy_Role" {
+resource "aws_iam_role" "codedeploy_service" {
   name = "codedeploy-service-role"
 
   assume_role_policy = <<EOF
