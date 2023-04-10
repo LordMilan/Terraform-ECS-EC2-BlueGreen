4bb51bc73682cad04962b951ebbf0f9f67138173
LordMilan <milan.mahat.739@gmail.com>
2023-04-10 13:58:19 +0545

    Code Deploy Added

--- listener_and_target_group.tf
+++ listener_and_target_group.tf
@@ -1,5 +1,5 @@
 
-## Target Group for APP is created here ##
+## Target Group for APP_BLUE is created here ##
 resource "aws_lb_target_group" "blue" {
   name        = "${local.tg_name_app}-blue"
   port        = "80"
@@ -14,10 +14,34 @@ resource "aws_lb_target_group" "blue" {
     interval            = 300
     matcher             = "200,301,302"
   }
+
+  depends_on = [aws_alb.existing]
 }
 
+## Listener Rule for APP_BLUE is created here ##
+resource "aws_lb_listener_rule" "app" {
+ depends_on = [local.alb_listener_arn]
+    lifecycle {
+    create_before_destroy = true
+  }
+  listener_arn = local.alb_listener_arn
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.blue.arn
+  }
+  condition {
+    host_header {
+      values = ["${local.app_url}"]
+    }
+  }
+  # condition {
+  #   path_pattern {
+  #     values = ["/your_path"]
+  #   }
+  # }
+}
 
-## Target Group for APP is created here ##
+## Target Group for APP_GREEN is created here ##
 resource "aws_lb_target_group" "green" {
   name        = "${local.tg_name_app}-green"
   port        = "80"
@@ -32,4 +56,6 @@ resource "aws_lb_target_group" "green" {
     interval            = 300
     matcher             = "200,301,302"
   }
+
+  depends_on = [aws_alb.existing]
 }
